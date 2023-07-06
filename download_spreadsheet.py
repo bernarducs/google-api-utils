85b4f3fc28c4cde7287eb8b0fd6ca8fd72ef9790
bernarducs <bernarducs@gmail.com>
2023-07-06 13:44:34 -0300

    criando cli para download de planilhas

---
+++ download_spreadsheet.py
@@ -0,0 +1,7 @@
+from src import download_spreadsheet
+import fire
+
+
+
+if __name__ == '__main__':
+    fire.Fire(download_spreadsheet)
--- main.py
+++
@@ -1,7 +0,0 @@
-from src import list_files, download_spreadsheet, export_dataset
-import fire
-
-
-
-if __name__ == '__main__':
-    fire.Fire(list_files)
